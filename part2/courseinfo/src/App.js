5a147cb8bd05827f6a3bf5f1f700d79cda58d965
MooCodes <60076255+MooCodes@users.noreply.github.com>
2021-09-02 15:32:21 -0500

    Exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,43 +1,4 @@
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const total = course.parts.reduce((acc, curr) => acc + curr.exercises, 0)
-  return (
-    <p><strong>total of {total} exercises</strong></p>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => 
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
