92db5381bdc8d760cb5566f3029ae12a057eeb67
MooCodes <60076255+MooCodes@users.noreply.github.com>
2021-09-02 12:57:00 -0500

    Exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,14 +11,43 @@ const Button = ({ onClick, text }) => {
 const StatisticLine = ({ text, value }) => {
   if (text === 'positive')
     return (
-      <div>
-        {text} {value} %
-      </div>
+      <>
+        <td>{text}</td><td>{value} %</td>
+      </>
     )
-    
+
+  return (
+    <>
+      <td>{text}</td><td>{value}</td>
+    </>
+  )
+}
+
+const StatisticTable = ({ data }) => {
   return (
     <div>
-      {text} {value}
+      <table>
+        <tbody>
+        <tr>
+          <StatisticLine text='good' value={data.good} />
+        </tr>
+        <tr>
+          <StatisticLine text='neutral' value={data.neutral} />
+        </tr>
+        <tr>
+          <StatisticLine text='bad' value={data.bad} />
+        </tr>
+        <tr>
+          <StatisticLine text='all' value={data.total} />
+        </tr>
+        <tr>
+          <StatisticLine text='average' value={data.average} />
+        </tr>
+        <tr>
+          <StatisticLine text='positive' value={data.positive} />
+        </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
@@ -31,14 +60,24 @@ const Statistics = ({good, neutral, bad }) => {
   if (total === 0)
     return <p>No feedback given</p>
 
+  let data = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    total: total,
+    average: average,
+    positive: posFeedbackPercentage
+  }
+
   return (
     <div>
-      <StatisticLine text='good' value={good} />
+      <StatisticTable data={data}/>
+      {/* <StatisticLine text='good' value={good} />
       <StatisticLine text='neutral' value={neutral} />
       <StatisticLine text='bad' value={bad} />
       <StatisticLine text='all' value={total} />
       <StatisticLine text='average' value={average} />
-      <StatisticLine text='positive' value={posFeedbackPercentage} />
+      <StatisticLine text='positive' value={posFeedbackPercentage} /> */}
     </div>
   )
 }
