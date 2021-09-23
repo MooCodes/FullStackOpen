f7bdc1f6ad08227c41ca82a2749d66bc5f5c430e
MooCodes <60076255+MooCodes@users.noreply.github.com>
2021-09-23 15:56:24 -0500

    Exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -8,8 +8,7 @@ import axios from 'axios'
 const Button = (props) => {
   let country = props.country
   let setSearchTerm = props.setSearchTerm
-  const handleClick = (e) =>
-    setSearchTerm(props.country.name)
+  const handleClick = (e) => setSearchTerm(country.name)
 
   return (
     <div>
@@ -20,7 +19,8 @@ const Button = (props) => {
 
 const Results = (props) => {
   console.log(props, "props")
-  let data = props.results
+  let data = props.results.countryData
+  let weatherData = props.results.weatherData
   let setSearchTerm = props.setSearchTerm
 
   // limit more than 10 results
@@ -60,17 +60,22 @@ const Results = (props) => {
       <h2>languages</h2>
       <ul>
         {
-          data[0].languages.map(lang => <li>{lang.name}</li>)
+          data[0].languages.map(lang => <li key={lang.name}>{lang.name}</li>)
         }
       </ul>
-      <img src={data[0].flag} height="100px" width="100px" />
+      <img src={data[0].flags[1]} height="100px" width="100px" />
+
+      <h2>Weather in {data[0].capital}</h2>
+      <p><strong>temperature:</strong> {weatherData.current.temperature} Celcius</p>
+      <img src={weatherData.current.weather_icons[0]} />
+      <p><strong>wind: </strong>{weatherData.current.wind_speed} mph direction {weatherData.current.wind_dir}</p>
     </div>
   )
 }
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('')
-  const [results, setResults] = useState([])
+  const [results, setResults] = useState({ countryData: [], weatherData: [] })
 
   const handleChange = (e) => setSearchTerm(e.target.value)
 
@@ -78,18 +83,29 @@ function App() {
   useEffect(() => {
     // empty searchTerm, so don't make request
     if (searchTerm !== '') {
+      // get country data
       axios
         .get(`https://restcountries.com/v2/name/${searchTerm}`)
-        .then(res => {
+        .then(countryRes => {
           // got response
-          console.log(res.data)
+          console.log(countryRes.data, 'got country data')
 
-          setResults(res.data)
+          // get weather data
+          axios
+            .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${countryRes.data[0].name}`)
+            .then(weatherRes => {
+              console.log(weatherRes.data, 'weather')
+              console.log('setting results!')
+              setResults({countryData: countryRes.data, weatherData: weatherRes.data})
+          })
       })
+
     }
   }, [searchTerm]) // run whenever the searchTerm changes
 
-  if (results.length > 0) {
+
+  if (results.countryData.length > 0) {
+    console.log('yo??')
     return (
       <div>
         find countries <input type="text" onChange={handleChange} />
